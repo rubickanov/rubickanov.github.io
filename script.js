a223a14b99821f218c73132900bc00f2be047ba7
rubickanov <rubickanov@gmai.com>
2024-06-12 22:22:32 +0400

    Improved dropdowns

--- script.js
+++ script.js
@@ -4,13 +4,30 @@ document.addEventListener('DOMContentLoaded', function() {
 
 document.querySelectorAll('.dropdown-button').forEach(button => {
     button.addEventListener('click', () => {
-        const content = button.previousElementSibling;
-        if (content.style.maxHeight) {
-            content.style.maxHeight = null;
-            button.classList.remove('open');
-        } else {
-            content.style.maxHeight = content.scrollHeight + "px";
-            button.classList.add('open');
-        }
+        toggleDescription(button);
     });
 });
+
+// Select all project logos
+const projectLogos = document.querySelectorAll('.project img');
+
+// Loop through each logo
+projectLogos.forEach((logo) => {
+    // Add click event listener
+    logo.addEventListener('click', (event) => {
+        // Find the corresponding button
+        const button = event.target.parentNode.querySelector('.dropdown-button');
+        toggleDescription(button);
+    });
+});
+
+function toggleDescription(button) {
+    const content = button.previousElementSibling;
+    if (content.style.maxHeight) {
+        content.style.maxHeight = null;
+        button.classList.remove('open');
+    } else {
+        content.style.maxHeight = content.scrollHeight + "px";
+        button.classList.add('open');
+    }
+}
